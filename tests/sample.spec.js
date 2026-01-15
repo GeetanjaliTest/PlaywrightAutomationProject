58964427ebeda3de627cb1dc4f8c7caacf4fce59
giitanjalim@gmail.com <giitanjalim@gmail.com>
2026-01-15 15:29:57 +0530

    Made changes to sample.spec.js

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -9,10 +9,18 @@ test('My First Test', async function({page}){
     expect(12).toBe(12);
 });
 
-test('My Second Test', async function({page}){
+test.skip('My Second Test', async function({page}){
+    //skip this test only
     expect(12).toBe(121);
 });
 
 test('My Third Test', async function({page}){
     expect(202).toBe(202);
+    expect(true).toBeTruthy();
+});
+
+test.only('My Fourth Test', async function({page}){
+    //only to run this test only
+    expect("Geeta").toContain('Geet');
+    expect(false).toBeFalsy();
 });
