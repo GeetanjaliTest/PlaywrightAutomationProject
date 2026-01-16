e902de77a656fc62a6bcc5ce29e84f17b02d327c
giitanjalim@gmail.com <giitanjalim@gmail.com>
2026-01-16 11:44:37 +0530

    Login spec

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,2 +1,13 @@
-const{test, expect}= require('@playwright/test')
+const { test, expect } = require('@playwright/test');
 
+test.only('Valid Login', async ({ page }) => {
+  await page.goto('/');
+
+  const url = page.url();
+  console.log('URL is:', url);
+
+  await expect(page).toHaveTitle('Regus Online Account');
+
+  await page.getByRole('button', { name: 'Login or register' }).click();
+
+});
